3f5767334de8141005e2edad4b9a0950473d5ee2
sapphire-al2o3 <t_telkun@hotmail.com>
2024-11-15 11:46:10 +0900

    スクロールビューにする

--- Assets/Scenes/CheckGraphicsFeature.cs
+++ Assets/Scenes/CheckGraphicsFeature.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 
 public class CheckGraphicsFeature : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+	Vector2 scrollPosition;
 
 	private void OnGUI()
 	{
-		GUILayout.Label($"supportsAsyncCompute: {SystemInfo.supportsAsyncCompute}");
-		GUILayout.Label($"supportsAsyncGPUReadback: {SystemInfo.supportsAsyncGPUReadback}");
-		GUILayout.Label($"supportedRenderTargetCount: {SystemInfo.supportedRenderTargetCount}");
-		GUILayout.Label($"supportsGeometryShaders: {SystemInfo.supportsGeometryShaders}");
-		GUILayout.Label($"supportsIndirectArgumentsBuffer: {SystemInfo.supportsIndirectArgumentsBuffer}");
-		GUILayout.Label($"supportsInstancing: {SystemInfo.supportsInstancing}");
-		GUILayout.Label($"supportsMipStreaming: {SystemInfo.supportsMipStreaming}");
-		GUILayout.Label($"supportsMultisampleAutoResolve: {SystemInfo.supportsMultisampleAutoResolve}");
-		GUILayout.Label($"supportsMultisampled2DArrayTextures: {SystemInfo.supportsMultisampled2DArrayTextures}");
-		GUILayout.Label($"supportsMultisampledTextures: {SystemInfo.supportsMultisampledTextures}");
-		GUILayout.Label($"supportsMultisampleResolveDepth: {SystemInfo.supportsMultisampleResolveDepth}");
-		GUILayout.Label($"supportsMultisampleResolveStencil: {SystemInfo.supportsMultisampleResolveStencil}");
+		using (new GUILayout.ScrollViewScope(scrollPosition))
+		{
+			GUILayout.Label($"graphicsDeviceName: {SystemInfo.graphicsDeviceName}");
+			GUILayout.Label($"graphicsDeviceVendor: {SystemInfo.graphicsDeviceVendor}");
 
+			GUILayout.Label($"supportsMotionVectors: {SystemInfo.supportsMotionVectors}");
+			GUILayout.Label($"supportsAsyncCompute: {SystemInfo.supportsAsyncCompute}");
+			GUILayout.Label($"supportsAsyncGPUReadback: {SystemInfo.supportsAsyncGPUReadback}");
+			GUILayout.Label($"supportedRenderTargetCount: {SystemInfo.supportedRenderTargetCount}");
+			GUILayout.Label($"supportsGeometryShaders: {SystemInfo.supportsGeometryShaders}");
+			GUILayout.Label($"supportsIndirectArgumentsBuffer: {SystemInfo.supportsIndirectArgumentsBuffer}");
+			GUILayout.Label($"supportsInstancing: {SystemInfo.supportsInstancing}");
+			GUILayout.Label($"supportsMipStreaming: {SystemInfo.supportsMipStreaming}");
+			GUILayout.Label($"supportsMultisampleAutoResolve: {SystemInfo.supportsMultisampleAutoResolve}");
+			GUILayout.Label($"supportsMultisampled2DArrayTextures: {SystemInfo.supportsMultisampled2DArrayTextures}");
+			GUILayout.Label($"supportsMultisampledTextures: {SystemInfo.supportsMultisampledTextures}");
+			GUILayout.Label($"supportsMultisampleResolveDepth: {SystemInfo.supportsMultisampleResolveDepth}");
+			GUILayout.Label($"supportsMultisampleResolveStencil: {SystemInfo.supportsMultisampleResolveStencil}");
+			GUILayout.Label($"supportedRandomWriteTargetCount: {SystemInfo.supportedRandomWriteTargetCount}");
+			GUILayout.Label($"supports2DArrayTextures: {SystemInfo.supports2DArrayTextures}");
+			GUILayout.Label($"supports32bitsIndexBuffer: {SystemInfo.supports32bitsIndexBuffer}");
+		}
 	}
 }
